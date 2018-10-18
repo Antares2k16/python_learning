4ec81edd07f448c6b140ac6652ede4d40601c9fd
Aaron Liao <aaron.liao17@gmail.com>
2018-10-17 21:24:25 -0700

    Bowling scores

--- babybowler.py
+++ babybowler.py
@@ -1,8 +1,8 @@
 import sys
 
 a, b, c = sys.argv[1:4]
-s = a + b + c
+s = int(a) + int(b) + int(c)
 
-print("Average of the 3 games: ", mean(s))
-print("Series of the 3 games: ", s)
-print("Handicap: ", + int(0.9 * (220-m)))
+print("Average of the 3 games: " + str(s/3))
+print("Series of the 3 games: " + str(s))
+print("Handicap: " + str(max(0, int(0.9 * (220-s/3)))))
