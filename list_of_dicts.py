8e2b86d50dac51fa1022e64aed1eda054407e1e8
Aaron Liao <aaron.liao17@gmail.com>
2018-10-24 21:24:04 -0700

    print statement error

---
+++ list_of_dicts.py
@@ -0,0 +1,12 @@
+fido = {
+        "type": "dog",
+        "owner": "sassy",
+        },
+lulu = {
+        "type": "cat",
+        "owner": "lala",
+        }
+pets = [fido, lulu]
+
+for p in pets:
+    print(p["type"])
