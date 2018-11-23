35c6f6f4887a9429bf1056fb7de6e3d6438e147a
Aaron Liao <aaron.liao17@gmail.com>
2018-11-23 15:58:46 -0800

    Employee class used in tests

---
+++ employee_info.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 23 14:49:13 2018
+
+@author: aliao
+"""
+
+class Employee():
+    def __init__(first_name, last_name, salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+        
+    def give_raise(self, boost=5000):
+        self.boost = boost
+        self.salary += self.boost
+    
