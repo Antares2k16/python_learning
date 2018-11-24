86c4c77787f99527b62f302988cb8cd5e90770a7
Aaron Liao <aaron.liao17@gmail.com>
2018-11-23 16:37:36 -0800

    Added self

--- employee_info.py
+++ employee_info.py
@@ -7,7 +7,7 @@ Created on Fri Nov 23 14:49:13 2018
 """
 
 class Employee():
-    def __init__(first_name, last_name, salary):
+    def __init__(self, first_name, last_name, salary):
         self.first_name = first_name
         self.last_name = last_name
         self.salary = salary
